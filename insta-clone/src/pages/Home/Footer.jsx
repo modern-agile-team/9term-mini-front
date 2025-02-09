ae2518833c47e4ee31f1980daea3c047758ddfbe
yihimin <yihimin01@gmail.com>
2025-02-09 17:30:22 +0900

    footer: border-r

--- insta-clone/src/pages/Home/Footer.jsx
+++ insta-clone/src/pages/Home/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
   };
 
   return (
-    <footer className="fixed bottom-0 w-full max-w-[768px] h-14 bg-white border-t border-gray-300 flex items-center justify-end px-4 mx-auto">
+    <footer className="fixed bottom-0 w-full max-w-[768px] h-14 bg-white border-t border-r border-gray-300 flex items-center justify-end px-4 mx-auto">
       {/* 우측: 로그아웃 버튼 */}
       <button onClick={handleLogout} className="flex items-center">
         <img src="/assets/icons/logout.svg" alt="logout" className="w-8 h-8" />
