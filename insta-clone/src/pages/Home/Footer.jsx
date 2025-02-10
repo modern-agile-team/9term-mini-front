b0725df206949629fdc567e19302eab14ffd7cf4
yihimin <yihimin01@gmail.com>
2025-02-10 11:08:12 +0900

    logout cursor

--- insta-clone/src/pages/Home/Footer.jsx
+++ insta-clone/src/pages/Home/Footer.jsx
@@ -11,7 +11,10 @@ const Footer = () => {
   return (
     <footer className="fixed bottom-0 w-full max-w-[768px] h-14 bg-white border-t border-r border-gray-300 flex items-center justify-end px-4 mx-auto">
       {/* 우측: 로그아웃 버튼 */}
-      <button onClick={handleLogout} className="flex items-center">
+      <button
+        onClick={handleLogout}
+        className="flex items-center cursor-pointer"
+      >
         <img src="/assets/icons/logout.svg" alt="logout" className="w-8 h-8" />
       </button>
     </footer>
