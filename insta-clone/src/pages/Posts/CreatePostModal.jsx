9ae48885305b848850864f56286e76f5aa18b937
eric5025 <kweon5025@gmail.com>
2025-02-17 11:25:23 +0900

    🐛bug fix(#14): uploadmodal-feedback

--- insta-clone/src/pages/Posts/CreatePostModal.jsx
+++ insta-clone/src/pages/Posts/CreatePostModal.jsx
@@ -74,24 +74,31 @@ const CreatePostModal = ({ onClose }) => {
       if (
         window.confirm('작성 중인 내용이 삭제됩니다. 그래도 나가시겠습니까?')
       ) {
+        onClose();  
+      } 
+    }else {
         onClose();
       }
-    } else {
-      onClose();
-    }
+    
   };
 
   return (
-    <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50"
+    onClick={handleCancel} >
+     <button
+      onClick={handleCancel}
+      className="absolute top-3 right-3"
+      > 
+    <img 
+      src="/assets/icons/cancel.svg" 
+      className=" w-7 h-7 hover:opacity-60 transition-colors brightness-0 invert-[1]"  // invert 클래스 사용
+     />
+</button>
+      
       <div className="bg-white w-full max-w-[500px] rounded-xl h-[min(90vh,500px)] flex flex-col">
         {/* 헤더 */}
         <div className="relative flex items-center justify-center p-3 border-b border-gray-200">
-          <button
-            onClick={handleCancel}
-            className="absolute left-3 text-sm font-medium text-black hover:text-gray-600 transition-colors"
-          >
-            취소
-          </button>
+        
           <h2 className="text-base font-semibold">새 게시물 만들기</h2>
           {selectedImage && (
             <button
@@ -125,12 +132,16 @@ const CreatePostModal = ({ onClose }) => {
                   />
                 </div>
                 {/* 글쓰기 영역 */}
-                <div className="border-t w-full p-3">
+                <div className=" w-full mt-2 ">
                   <textarea
                     value={caption}
                     onChange={e => setCaption(e.target.value)}
                     placeholder="문구 입력..."
-                    className="w-full h-24 resize-none border-none focus:ring-0 focus:outline-none text-sm"
+                    className="w-full h-25 resize-none
+                    bg-[#fafafa]
+                    border border-gray-300 rounded-sm focus:outline-none
+                    focus:border-gray-400
+                    placeholder:text-sm"
                     maxLength={500}
                   />
                   <div className="flex justify-end items-center text-sm text-gray-500">
