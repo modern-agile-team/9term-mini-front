19275c11a18f10b29c5f9c3edb638750f99b3a84
yihimin <yihimin01@gmail.com>
2025-02-17 10:58:21 +0900

    fix: login.jsx, signup.jsx api 경로

--- insta-clone/src/pages/Auth/Login.jsx
+++ insta-clone/src/pages/Auth/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
       const response = await fetch('/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email, pwd }),
       });
 
       const data = await response.json();
--- insta-clone/src/pages/Auth/Signup.jsx
+++ insta-clone/src/pages/Auth/Signup.jsx
@@ -33,7 +33,7 @@ const Signup = () => {
       const response = await fetch('/api/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email, pwd }),
       });
 
       const data = await response.json();
