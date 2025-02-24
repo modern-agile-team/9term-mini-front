0854c340dd325d81e8dbfcabb1d95ca6840fd0f7
yihimin <yihimin01@gmail.com>
2025-02-24 15:45:11 +0900

    refactor:../를 @절대경로로 수정

--- insta-clone/src/pages/Auth/Signup.jsx
+++ insta-clone/src/pages/Auth/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import logoSvg from '/assets/icons/logo.svg';
-import validateAuth from '../../utils/utils';
+import validateAuth from '@/utils/utils';
 import apiClient from '@/services/apiClient';
 
 const Signup = () => {
